d23b70d9132e67f4698bc84f2fb7cbaa7cf93dc0
seoljjang <seoljjang00@gmail.com>
2020-10-10 20:27:09 +0900

    qna 페이지 넘기기 수정

--- js/main.js
+++ js/main.js
@@ -1,60 +1,67 @@
 const start_btn = document.querySelector(".start-btn");
 const hero = document.querySelector(".hero");
 const qna = document.querySelector(".qna");
+const page = document.querySelector(".qna__page");
+const pageNum = document.createElement("span");
 const loading = document.querySelector(".loading");
 const result = document.querySelector(".result");
 
 const SELECT = [];
-const ENDPOINT = 3;
-let qIndex = -1;
+let questionIndex = -1;
 
 const end = () => {
     qna.style.animation = "fade-out 0.3s forwards";
+    qna.style.display = "none";
     result.style.animation = "fade-in 0.3s forwards";
     setTimeout(()=> {
-        qna.style.display = "none";
         result.style.display = "block";
     }, 500);
 }
 
-//다른 분 블로그 보고 일단은 따라 해봤지만 아직 코드가 제대로 이해되지 않음.
-//천천히 뜯어보면서 이해하고 내가 편한 방법으로 새로 해보기
-
-const qnaAnswer = (answerText, index) => {
+const chooseAnswer = (answerText, index) => {
     const choiceBtn = document.createElement("button");
     const choiceContainer = document.querySelector(".qna__choice");
+
     choiceBtn.innerText = answerText;
-    choiceBtn.classList.add("choice-btn");
     choiceContainer.appendChild(choiceBtn);
 
     choiceBtn.addEventListener("click", () => {
         const parent = choiceContainer.parentNode;
-        const children = parent.childNodes;
-        for (let i in children) {
-            children[i].disabled = true;
+        const childrens = parent.childNodes;
+        for (let i in childrens) {
+            childrens[i].disabled = true;
         }
+        parent.style.animation = "fade-out 0.3s forwards";
         setTimeout(() => {
-            SELECT[qIndex] = index; 
-            choiceContainer.innerHTML = '';
-            qnaTitle();
-        }, 300);
+            SELECT[questionIndex] = index; //anwser 번호
+            choiceContainer.innerHTML = "";
+            parent.style.animation = "fade-in 0.3s forwards";
+            NextQuestion();
+        }, 500);
     });
 }
 
-const qnaTitle = () => {
-    if(qIndex++ === qnaList.length - 1) {
+
+const NextQuestion = () => {
+    if(questionIndex++ === qnaList.length - 1) {
         end();
         return;
     }
-    const qNum = qnaList[qIndex];
-    const qTitle = document.querySelector(".qna__title");
-    const currentPage = document.querySelector(".qna__page span:first-child");
 
-    qTitle.innerText = qNum.q;
+    pageNum.innerText = `${questionIndex + 1} / 10`;
+    page.appendChild(pageNum);
 
-    for (let i in qNum.a) {
-        qnaAnswer(qNum.a[i].answer, i);
+    const questionNum = qnaList[questionIndex];
+    const questionTitle = document.querySelector(".qna__title");
+    questionTitle.innerText = questionNum.q;
+
+    for(let i in questionNum.a) {
+        chooseAnswer(questionNum.a[i].answer, i);
     }
+}
+
+function init() {
+    NextQuestion();
 }
 
 const handleStart = () => {
@@ -64,7 +71,7 @@ const handleStart = () => {
         hero.style.display = "none";
         qna.style.display = "block";
     }, 500);
-    qnaTitle();
+    NextQuestion();
 }
 
 function init() {
